56ddd33a793e34f155b5f5d88b9edc26f3a7cb00
Beth <bethhnielsen56@gmail.com>
2020-03-04 18:14:19 -0600

    fight info made

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,10 +1,12 @@
 import '../styles/main.scss';
 import fullnessInfo from './components/eat';
 import playInfo from './components/play';
+import fightInfo from './components/fight';
 
 const init = () => {
   fullnessInfo.hunger();
   playInfo.play();
+  fightInfo.battle();
 };
 
 init();
